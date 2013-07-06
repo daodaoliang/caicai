e40926345be31cefdaa96c06217d6525bb7a15f9
liujian <xiaobingcaicai@126.com>
2013-07-06 14:42:51 +0800

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,8 @@
 #include <QSqlDatabase>
 #include <QDebug>
 #include "titlewidget.h"
-<<<<<<< HEAD
 #include "printerwidget.h"
-=======
 #include "sqlmanager.h"
->>>>>>> 3b228b217aa7912381fee3c04ce88c1b25093133
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -18,21 +15,11 @@ int main(int argc, char *argv[])
     QPalette palette;
     palette.setColor(QPalette::WindowText,Qt::white);
     a.setPalette(palette);
-<<<<<<< HEAD
-    //mainWidget w;
-    //w.show();
-    PrinterWidget myWidget;
-    myWidget.EnterWidget(&myWidget);
-    myWidget.show();
-=======
-
-  //  a.setFont(QFont());
-    mainWidget w;
-
-  //  sysButtonWidget w;
-  //  titleWidget w;
-    w.show();
-    getSqlManager()->Init();
->>>>>>> 3b228b217aa7912381fee3c04ce88c1b25093133
+//    mainWidget w;
+//    w.show();
+//    getSqlManager()->Init();
+    PrinterWidget testWidget;
+    testWidget.EnterWidget(&testWidget);
+    testWidget.show();
     return a.exec();
 }
