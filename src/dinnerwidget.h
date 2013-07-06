e005506c1d98467e5697b7d45f4b044273e275d7
wangxk <wangxk@eaglesoftware.cn>
2013-07-07 00:02:19 +0800

    完成餐桌的更新

--- src/dinnerwidget.h
+++ src/dinnerwidget.h
@@ -15,11 +15,22 @@ class DinnerWidget : public QWidget
 public:
     explicit DinnerWidget(QWidget *parent = 0);
     ~DinnerWidget();
+    void updateData();
 private:
     void startWx();
 private:
     Ui::DinnerWidget *ui;
     QSqlQueryModel *m_model;
 };
+
+inline DinnerWidget *dinnerWidget()
+{
+    static DinnerWidget *w = NULL;
+    if(w == NULL)
+    {
+        w = new DinnerWidget();
+    }
+    return w;
+}
 
 #endif // DINNERWIDGET_H
