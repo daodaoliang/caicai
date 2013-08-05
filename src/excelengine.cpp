ae04e66efd1f40c6aa5839d5517c60830ccb07b9
daodaoliang <15253129614@163.com>
2013-08-05 16:49:12 +0800

    dd

--- src/excelengine.cpp
+++ src/excelengine.cpp
@@ -246,7 +246,10 @@ bool ExcelEngine::SaveDataFrTable(QTableView *tableWidget)
         {
             if (!tableWidget->model()->index(i,j).data().toString().isEmpty())
             {
-                this->SetCellData(i+2,j+1,tableWidget->model()->index(i,j).data().toString());
+                QString tempItem=tableWidget->model()->index(i,j).data().toString();
+                if(tempItem.count()>=15)
+                    tempItem.push_front(QChar('\''));
+                this->SetCellData(i+2,j+1,tempItem);
             }
         }
     }
