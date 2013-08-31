5d953fccf8812ac415907930d0b849e99536b22b
wangxk <wangxk@eaglesoftware.cn>
2013-08-31 15:02:01 +0800

    完成第一个报表

--- report/report/report/Form1.Designer.cs
+++ report/report/report/Form1.Designer.cs
@@ -28,20 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode33 = new System.Windows.Forms.TreeNode("菜例销售排名");
-            System.Windows.Forms.TreeNode treeNode34 = new System.Windows.Forms.TreeNode("退菜查询");
-            System.Windows.Forms.TreeNode treeNode35 = new System.Windows.Forms.TreeNode("营业报表");
-            System.Windows.Forms.TreeNode treeNode36 = new System.Windows.Forms.TreeNode("结算报表", new System.Windows.Forms.TreeNode[] {
-            treeNode33,
-            treeNode34,
-            treeNode35});
+            System.Windows.Forms.TreeNode treeNode37 = new System.Windows.Forms.TreeNode("菜例销售排名");
+            System.Windows.Forms.TreeNode treeNode38 = new System.Windows.Forms.TreeNode("退菜查询");
+            System.Windows.Forms.TreeNode treeNode39 = new System.Windows.Forms.TreeNode("营业报表");
+            System.Windows.Forms.TreeNode treeNode40 = new System.Windows.Forms.TreeNode("结算报表", new System.Windows.Forms.TreeNode[] {
+            treeNode37,
+            treeNode38,
+            treeNode39});
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.结算报表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.会员卡报表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -75,16 +75,16 @@
                         | System.Windows.Forms.AnchorStyles.Left)));
             this.treeView1.Location = new System.Drawing.Point(12, 28);
             this.treeView1.Name = "treeView1";
-            treeNode33.Name = "节点1";
-            treeNode33.Text = "菜例销售排名";
-            treeNode34.Name = "节点4";
-            treeNode34.Text = "退菜查询";
-            treeNode35.Name = "节点5";
-            treeNode35.Text = "营业报表";
-            treeNode36.Name = "节点0";
-            treeNode36.Text = "结算报表";
+            treeNode37.Name = "节点1";
+            treeNode37.Text = "菜例销售排名";
+            treeNode38.Name = "节点4";
+            treeNode38.Text = "退菜查询";
+            treeNode39.Name = "节点5";
+            treeNode39.Text = "营业报表";
+            treeNode40.Name = "节点0";
+            treeNode40.Text = "结算报表";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode36});
+            treeNode40});
             this.treeView1.Size = new System.Drawing.Size(165, 329);
             this.treeView1.TabIndex = 1;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
@@ -93,10 +93,10 @@
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(617, 29);
+            this.groupBox1.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(606, 29);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(171, 328);
+            this.groupBox1.Size = new System.Drawing.Size(182, 328);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "查询条件";
@@ -108,18 +108,16 @@
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.reportViewer1.Location = new System.Drawing.Point(183, 29);
             this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.Size = new System.Drawing.Size(428, 328);
+            this.reportViewer1.Size = new System.Drawing.Size(417, 328);
             this.reportViewer1.TabIndex = 4;
             // 
-            // button1
+            // flowLayoutPanel1
             // 
-            this.button1.Location = new System.Drawing.Point(6, 143);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 17);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(176, 308);
+            this.flowLayoutPanel1.TabIndex = 0;
             // 
             // Form1
             // 
@@ -150,7 +148,7 @@
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.GroupBox groupBox1;
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
 
--- report/report/report/Form1.cs
+++ report/report/report/Form1.cs
@@ -11,7 +11,7 @@ namespace report
 {
     public partial class Form1 : Form
     {
-        enum QueryType
+        public enum QueryType
         {
             PaiMing,
             TuiCai,
@@ -27,10 +27,10 @@ namespace report
         private void Form1_Load(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Maximized;
-
+            initUi();
         }
 
-        private string[] reportNames = { "report.Report1.rdlc" ,
+        private string[] m_reportNames = { "report.Report1.rdlc" ,
                                        "report.TuiCai.rdlc",
                                        "report.Yingye.rdlc"};
 
@@ -51,10 +51,26 @@ namespace report
                     break;
             }
         }
+        private void showQueryTime()
+        {
+            flowLayoutPanel1.Controls.Clear();
+            flowLayoutPanel1.Controls.Add(m_from);
+            flowLayoutPanel1.Controls.Add(m_fromPicker);
+            flowLayoutPanel1.Controls.Add(m_to);
+            flowLayoutPanel1.Controls.Add(m_toPicker);            
+        }
 
         //显示排名信息
         private void showPaiMing()
         {
+            //显示查询条件
+            showQueryTime();      
+            //个性化查询条件控件
+            m_label1.Text = "类别";
+            flowLayoutPanel1.Controls.Add(m_label1);
+            flowLayoutPanel1.Controls.Add(m_query1);
+            flowLayoutPanel1.Controls.Add(m_queryButton);
+            //设置包表类型
             m_queryType = QueryType.PaiMing;
         }
 
@@ -70,10 +86,67 @@ namespace report
             m_queryType = QueryType.YingYe;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+        private DataProvider m_dataProvider = new DataProvider();
+        private TextBox m_query1 = new TextBox();
+        private TextBox m_query2 = new TextBox();
+        private TextBox m_query3 = new TextBox();
+        private TextBox m_query4 = new TextBox();
+        private TextBox m_query5 = new TextBox();
+        private Label m_label1 = new Label();
+        private Label m_label2 = new Label();
+        private Label m_label3 = new Label();
+        private Label m_label4 = new Label();
+        private Label m_label5 = new Label();
+        private Label m_from = new Label();
+        private Label m_to = new Label();
+        private DateTimePicker m_fromPicker = new DateTimePicker();
+        private DateTimePicker m_toPicker = new DateTimePicker();
+        private Button m_queryButton = new Button();
+        private void initUi()
         {
+            m_label2.Size = new Size(55, 23);
+            m_label1.Size = new Size(55, 23);
+            m_label3.Size = new Size(55, 23);
+            m_label4.Size = new Size(55, 23);
+            m_label5.Size = new Size(55, 23);
+            m_to.Size = new Size(55, 23);
+            m_from.Size = new Size(55, 23);
+            m_toPicker.Size = new System.Drawing.Size(105, 30);
+            m_fromPicker.Size = new System.Drawing.Size(105, 30);
+            m_query1.Size = new System.Drawing.Size(105, 30);
+            m_query2.Size = new System.Drawing.Size(105, 30);
+            m_query3.Size = new System.Drawing.Size(105, 30);
+            m_query4.Size = new System.Drawing.Size(105, 30);
+            m_query5.Size = new System.Drawing.Size(105, 30);
+            m_to.Text = "到";
+            m_from.Text = "从";
+            m_label1.TextAlign = ContentAlignment.MiddleRight;
+            m_label2.TextAlign = ContentAlignment.MiddleRight;
+            m_label3.TextAlign = ContentAlignment.MiddleRight;
+            m_label4.TextAlign = ContentAlignment.MiddleRight;
+            m_label5.TextAlign = ContentAlignment.MiddleRight;
+            m_to.TextAlign = ContentAlignment.MiddleRight;
+            m_from.TextAlign = ContentAlignment.MiddleRight;
+            m_queryButton.Text = "查询";
+            m_queryButton.Click += new EventHandler(m_queryButton_Click);    
+        }
+
+        void m_queryButton_Click(object sender, EventArgs e)
+        {
+            string[] queryList = new string[5];
+            queryList[0] = m_query1.Text;
+            queryList[1] = m_query2.Text;
+            queryList[2] = m_query3.Text;
+            queryList[3] = m_query4.Text;
+            queryList[4] = m_query5.Text;
+            string beginTime = m_fromPicker.Text;
+            string endTime = m_toPicker.Text;
+            DataTable dt = m_dataProvider.getData(m_queryType, beginTime, endTime, queryList);
             reportViewer1.Reset();
-            reportViewer1.LocalReport.ReportEmbeddedResource = reportNames[(int)m_queryType];
+            reportViewer1.LocalReport.ReportEmbeddedResource = m_reportNames[(int)m_queryType];
+            reportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WinForms.ReportDataSource(
+                "DataSet2", dt));
             reportViewer1.RefreshReport();
         }
     }
